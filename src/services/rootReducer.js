6f87721f05b090fa81afb2d78a9bc7e795bd162f
NakamuraSuzuki <devprodev777@gmail.com>
2022-02-14 14:28:55 +0100

    Delete src directory

--- src/services/rootReducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-//import userReducer from "./user/userReducer";
-import authReducer from "./user/auth/authReducer";
-
-const rootReducer = combineReducers({
-  auth: authReducer,
-});
-
-export default rootReducer;
--- src/services/user/auth/authTypes.js
+++
@@ -1,8 +0,0 @@
-export const LOGIN_REQUEST = "LOGIN_REQUEST";
-export const REGISTER_REQUEST = "REGISTER_REQUEST";
-export const LOGOUT_REQUEST = "LOGOUT_REQUEST";
-
-export const SUCCESS = "SUCCESS";
-export const FAILURE = "FAILURE";
-export const SUCCESS_R = "SUCCESS_R";
-export const FAILURE_R = "FAILURE_R";
